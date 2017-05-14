d310e64813a1b4ec6cb5ad334414d714fd4afe49
Furacin <manzanile@gmail.com>
2017-05-15 01:06:00 +0200

    Preparamos para añadir el NavigationView
    Añadido Toolbar

--- app/src/main/java/com/furazin/android/mbandgsr/MainActivity.java
+++ app/src/main/java/com/furazin/android/mbandgsr/MainActivity.java
@@ -3,7 +3,9 @@ package com.furazin.android.mbandgsr;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -47,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         Button NuevoForm = (Button) findViewById(R.id.FormularioButton);
         NuevoForm.setOnClickListener(new View.OnClickListener() {
