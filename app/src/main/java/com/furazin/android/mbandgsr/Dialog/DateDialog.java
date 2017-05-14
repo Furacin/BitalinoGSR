f3312e448e0dedaec7685f1beb7228355dc759fc
Furacin <manzanile@gmail.com>
2017-05-14 18:01:36 +0200

    Ahora aparece el DialogPicker al pulsar tanto el botón como el EditText

--- app/src/main/java/com/furazin/android/mbandgsr/Dialog/DateDialog.java
+++ app/src/main/java/com/furazin/android/mbandgsr/Dialog/DateDialog.java
@@ -5,9 +5,9 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.DatePicker;
-import android.widget.EditText;
+
+import com.furazin.android.mbandgsr.Formulario;
 
 import java.util.Calendar;
 
@@ -18,10 +18,10 @@ import java.util.Calendar;
 @SuppressLint("ValidFragment")
 public class DateDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-    EditText txtDate;
-    public DateDialog(View view) {
-        txtDate = (EditText)view;
-    }
+//    EditText txtDate;
+//    public DateDialog(View view) {
+//        txtDate = (EditText)view;
+//    }
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Calendar c = Calendar.getInstance();
@@ -34,6 +34,6 @@ public class DateDialog extends DialogFragment implements DatePickerDialog.OnDat
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
         String date = day + "-" + (month+1) + "-" + year;
-        txtDate.setText(date);
+        Formulario.txtDate.setText(date);
     }
 }
--- app/src/main/java/com/furazin/android/mbandgsr/Formulario.java
+++ app/src/main/java/com/furazin/android/mbandgsr/Formulario.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.furazin.android.mbandgsr.Dialog.DateDialog;
 
@@ -13,20 +14,32 @@ import com.furazin.android.mbandgsr.Dialog.DateDialog;
 
 public class Formulario extends AppCompatActivity{
 
+    public static EditText txtDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        EditText txtDate = (EditText)findViewById(R.id.editTextFecha);
+        txtDate = (EditText)findViewById(R.id.editTextFecha);
         txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
                 if (b) {
-                    DateDialog dialog = new DateDialog(view);
+                    DateDialog dialog = new DateDialog();// = new DateDialog(view);
                     android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
                     dialog.show(ft, "DatePicker");
                 }
+            }
+        });
+
+        ImageButton dateIcon = (ImageButton) findViewById(R.id.dateButton);
+        dateIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DateDialog dialog = new DateDialog(); //= new DateDialog(view);
+                android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                dialog.show(ft, "DatePicker");
             }
         });
 
