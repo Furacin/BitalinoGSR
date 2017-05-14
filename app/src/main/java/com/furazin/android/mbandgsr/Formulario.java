c616c7a00ea7cd37e63ff4893de1ec46db9df454
Furacin <manzanile@gmail.com>
2017-05-14 15:10:38 +0200

    Añadido DialogPicker con la fecha al pulsar en EditText del formulario.

--- app/src/main/java/com/furazin/android/mbandgsr/Formulario.java
+++ app/src/main/java/com/furazin/android/mbandgsr/Formulario.java
@@ -2,17 +2,43 @@ package com.furazin.android.mbandgsr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.furazin.android.mbandgsr.Dialog.DateDialog;
 
 /**
  * Created by manza on 11/05/2017.
  */
 
-public class Formulario extends AppCompatActivity {
+public class Formulario extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
+        EditText txtDate = (EditText)findViewById(R.id.editTextFecha);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    DateDialog dialog = new DateDialog(view);
+                    android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
+                }
+            }
+        });
+
+//        ImageButton dateButton = (ImageButton) findViewById(R.id.dateButton);
+//        dateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DateDialog dialog = new DateDialog(view);
+//                android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+//                dialog.show(ft,"DatePicker");
+//            }
+//        });
     }
+
 }
