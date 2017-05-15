d13043652654fef355e878c474c7f3ee1022d205
Furacin <manzanile@gmail.com>
2017-05-15 19:32:58 +0200

    El botón del formulario te lleva a la pantalla de datos de GSR
    Si hay algún campo del formulario sin rellenar, no te deja cambiar de activity y aparece un Toast informando de esto.

--- app/src/main/java/com/furazin/android/mbandgsr/Formulario.java
+++ app/src/main/java/com/furazin/android/mbandgsr/Formulario.java
@@ -1,10 +1,14 @@
 package com.furazin.android.mbandgsr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.furazin.android.mbandgsr.Dialog.DateDialog;
 
@@ -15,6 +19,8 @@ import com.furazin.android.mbandgsr.Dialog.DateDialog;
 public class Formulario extends AppCompatActivity{
 
     public static EditText txtDate;
+    EditText nombre, apellidos, fecha_nacimiento, descripcion;
+    RadioButton sexo_masculino, sexo_femenino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +49,31 @@ public class Formulario extends AppCompatActivity{
             }
         });
 
-//        ImageButton dateButton = (ImageButton) findViewById(R.id.dateButton);
-//        dateButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                DateDialog dialog = new DateDialog(view);
-//                android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
-//                dialog.show(ft,"DatePicker");
-//            }
-//        });
+        nombre = (EditText) findViewById(R.id.editTextNombre);
+        apellidos = (EditText) findViewById(R.id.editTextApellidos);
+        fecha_nacimiento = (EditText) findViewById(R.id.editTextFecha);
+        descripcion = (EditText) findViewById(R.id.text_descripcion);
+
+        sexo_femenino = (RadioButton) findViewById(R.id.radioButton1) ;
+        sexo_masculino = (RadioButton) findViewById(R.id.radioButton2) ;
+
+        Button ButtonInicio = (Button) findViewById(R.id.InicioPrueba_Button);
+        ButtonInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isEmpty(nombre) && !isEmpty(apellidos) && !isEmpty(fecha_nacimiento) && !isEmpty(descripcion)) {
+                    Intent i = new Intent(getApplicationContext(), DatosGSR.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No puede estar ningún campo del formulario vacío", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
     }
 
 }
